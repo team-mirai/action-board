9415ef6306c4a4fa820e8dc7b24accdf8fbd0692
TATSUYA KUROKAWA <thepekinensis@gmail.com>
2025-05-27 22:42:22 +0900

    remove duplicate secrets

--- terraform/nextjs-app/cloud_build.tf
+++ terraform/nextjs-app/cloud_build.tf
@@ -21,46 +21,6 @@ resource "google_project_iam_member" "cloudbuild_iam" {
 }
 
 
-# Supabase Database Password Secret
-resource "google_secret_manager_secret" "supabase_db_password" {
-  secret_id = "${var.app_name}-${var.environment}-SUPABASE_DB_PASSWORD"
-
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "supabase_db_password" {
-  secret      = google_secret_manager_secret.supabase_db_password.id
-  secret_data = var.SUPABASE_DB_PASSWORD
-}
-
-resource "google_secret_manager_secret" "supabase_service_role_key" {
-  secret_id = "${var.app_name}-${var.environment}-SUPABASE_SERVICE_ROLE_KEY"
-
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "supabase_service_role_key" {
-  secret      = google_secret_manager_secret.supabase_service_role_key.id
-  secret_data = var.SUPABASE_SERVICE_ROLE_KEY
-}
-
-resource "google_secret_manager_secret" "supabase_access_token" {
-  secret_id = "${var.app_name}-${var.environment}-SUPABASE_ACCESS_TOKEN"
-
-  replication {
-    auto {}
-  }
-}
-
-resource "google_secret_manager_secret_version" "supabase_access_token" {
-  secret      = google_secret_manager_secret.supabase_access_token.id
-  secret_data = var.SUPABASE_ACCESS_TOKEN
-}
-
 # Cloud Build サービスアカウントにシークレットアクセス権限を付与
 resource "google_secret_manager_secret_iam_member" "supabase_db_password_accessor" {
   secret_id = google_secret_manager_secret.supabase_db_password.secret_id
