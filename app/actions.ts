b261cc187717b8c5dbd86027fbf09b7388b60be1
Tomoaki Sano <ryan5500@gmail.com>
2025-06-01 18:17:21 +0900

    fix: パスワードリセットの際のリダイレクトリンクを修正

--- app/actions.ts
+++ app/actions.ts
@@ -207,7 +207,7 @@ export const forgotPasswordAction = async (formData: FormData) => {
   }
 
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${origin}/auth/callback?redirect_to=/protected/reset-password`,
+    redirectTo: `${origin}/auth/callback?redirect_to=/reset-password`,
   });
 
   if (error) {
