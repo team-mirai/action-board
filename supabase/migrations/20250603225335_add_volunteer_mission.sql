861dc82f03e644f465373be6b64606820b7614b7
tomoe <sakurai@tokyo-calendar.co.jp>
2025-06-04 08:41:57 +0900

    fix 文言変更

--- supabase/migrations/20250603225335_add_volunteer_mission.sql
+++ supabase/migrations/20250603225335_add_volunteer_mission.sql
@@ -4,7 +4,7 @@ VALUES
     gen_random_uuid(),
    'ボランティアに登録しよう',
     '/img/mission_fallback_icon.png',
-     '<a href="https://docs.google.com/forms/d/e/1FAIpQLScBqdkRNCRjy5OfLMM-drRWMCZDLrqx5YQVemhLsiH93KNq8g/viewform" target="_blank" rel="noopener noreferrer">ボランティア登録フォーム</a>からボランティア登録を行ってください。<br/>成果物として、登録したメールアドレスを入力してください。<br/>',
+     '<a href="https://docs.google.com/forms/d/e/1FAIpQLScBqdkRNCRjy5OfLMM-drRWMCZDLrqx5YQVemhLsiH93KNq8g/viewform" target="_blank" rel="noopener noreferrer">ボランティア登録フォーム</a>からボランティアに登録してください。<br/>成果物として、登録したメールアドレスを入力してください。<br/>',
       2,
        NULL,
         'TEXT',
