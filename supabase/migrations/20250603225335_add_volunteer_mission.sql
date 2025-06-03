65474c6d2961c9eeae46775b7b825089eb6f65ac
tomoe <sakurai@tokyo-calendar.co.jp>
2025-06-04 08:33:38 +0900

    add ボランティア登録ミッションを追加

---
+++ supabase/migrations/20250603225335_add_volunteer_mission.sql
@@ -0,0 +1,12 @@
+INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count)
+VALUES
+  (
+    gen_random_uuid(),
+   'ボランティアに登録しよう',
+    '/img/mission_fallback_icon.png',
+     '<a href="https://docs.google.com/forms/d/e/1FAIpQLScBqdkRNCRjy5OfLMM-drRWMCZDLrqx5YQVemhLsiH93KNq8g/viewform" target="_blank" rel="noopener noreferrer">ボランティア登録フォーム</a>からボランティア登録を行ってください。<br/>成果物として、登録したメールアドレスを入力してください。<br/>',
+      2,
+       NULL,
+        'TEXT',
+         1
+  )
