e13b3edc8bfffb41736800b77c30b7ad2157511f
tomoe <sakurai@tokyo-calendar.co.jp>
2025-06-04 11:51:46 +0900

    fix 難易度を１に変更、ogp_image_url, artifact_lavelを追加

--- supabase/migrations/20250603225335_add_volunteer_mission.sql
+++ supabase/migrations/20250603225335_add_volunteer_mission.sql
@@ -1,12 +1,14 @@
-INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count)
+INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count, ogp_image_url, artifact_lavel)
 VALUES
   (
     gen_random_uuid(),
-   'ボランティアに登録しよう',
+    'ボランティアに登録しよう',
     '/img/mission_fallback_icon.png',
-     '<a href="https://docs.google.com/forms/d/e/1FAIpQLScBqdkRNCRjy5OfLMM-drRWMCZDLrqx5YQVemhLsiH93KNq8g/viewform" target="_blank" rel="noopener noreferrer">ボランティア登録フォーム</a>からボランティアに登録してください。<br/>成果物として、登録したメールアドレスを入力してください。<br/>',
-      2,
-       NULL,
-        'TEXT',
-         1
+    '<a href="https://docs.google.com/forms/d/e/1FAIpQLScBqdkRNCRjy5OfLMM-drRWMCZDLrqx5YQVemhLsiH93KNq8g/viewform" target="_blank" rel="noopener noreferrer">ボランティア登録フォーム</a>からボランティアに登録してください。<br/>成果物として、登録したメールアドレスを入力してください。<br/>',
+    1,
+    NULL,
+    'TEXT',
+    1,
+    "",
+    "登録したメールアドレス"
   )
