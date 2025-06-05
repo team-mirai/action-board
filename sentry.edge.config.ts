43812ca323927e6cdcacd05d4956c2ad4e606156
koya1616 <koya1616.0622@gmail.com>
2025-06-05 14:49:26 +1200

    fix: update tracesSampleRate comments for clarity in Sentry configuration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -13,5 +13,10 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.1,
   });
 }
