88bb41d75027faca585ef8a074b8baae53bd4b32
IsseiToura <isseitoura0429@gmail.com>
2025-06-06 18:38:37 +1000

    ビルドエラーの解消

--- app/(auth-pages)/sign-up-success/page.tsx
+++ app/(auth-pages)/sign-up-success/page.tsx
@@ -1,12 +1,7 @@
 import { FormMessage } from "@/components/form-message";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function SignUpSuccess({
-  searchParams,
-}: {
-  searchParams: { message?: string };
-}) {
+export default function SignUpSuccess() {
   return (
     <div className="flex-1 flex flex-col min-w-72">
       <div className="flex justify-center items-center m-4">
