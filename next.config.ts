1f989517926392f1cdb94e0323bc04e0c25b8694
IsseiToura <isseitoura0429@gmail.com>
2025-06-06 18:02:14 +1000

    Revert "Merge remote-tracking branch 'upstream/develop' into develop"
    This reverts commit f22c38ded8882297d278d75cbd5970b7e73af6a7, reversing
    changes made to 77f8c472460fe8017f1a7dbcb4e5a28389801142.

--- next.config.ts
+++ next.config.ts
@@ -52,13 +52,4 @@ export default withSentryConfig(nextConfig, {
     ignore: ["**/node_modules/**", "**/.next/cache/**", "**/tests/**"],
     deleteSourcemapsAfterUpload: true,
   },
-
-  release: {
-    name: process.env.SENTRY_RELEASE,
-    setCommits: {
-      auto: true,
-      ignoreMissing: true,
-      ignoreEmpty: true,
-    },
-  },
 });
