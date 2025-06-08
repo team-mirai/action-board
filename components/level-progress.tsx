e1ed9293b7ae58914c291777bc0f844e48a6fdfd
muraikenta <muraikenta0507@gmail.com>
2025-06-08 22:04:17 +0900

    UI調整

--- components/level-progress.tsx
+++ components/level-progress.tsx
@@ -1,6 +1,7 @@
 import type { UserLevel } from "@/lib/services/userLevel";
 import { totalXp } from "@/lib/utils/utils";
 import React from "react";
+import { ProgressBarSimple } from "./ui/progress-bar-simple";
 import { ProgressCircle } from "./ui/progress-circle";
 
 interface LevelProgressProps {
@@ -24,44 +25,9 @@ export function LevelProgress({ userLevel }: LevelProgressProps) {
   const xpRangeForCurrentLevel = nextLevelRequiredXp - currentLevelStartXp;
 
   return (
-    <div className="flex flex-col items-center">
-      <ProgressCircle
-        current={progressInCurrentLevel}
-        max={xpRangeForCurrentLevel}
-        size={190}
-        strokeWidth={15}
-        animationDelay={200}
-        centerText={
-          <div className="text-center">
-            <div className="text-xs font-bold mb-1">次のレベルまで</div>
-            <div
-              className="text-5xl font-bold leading-none"
-              style={{ color: "#30BAA7" }}
-            >
-              {progressInCurrentLevel.toLocaleString()}
-            </div>
-            <div
-              className="text-xs font-bold leading-none mt-1"
-              style={{ color: "#30BAA7" }}
-            >
-              / {xpRangeForCurrentLevel.toLocaleString()}
-            </div>
-          </div>
-        }
-        centerTextClassName="px-4"
-      />
-
-      <div className="mt-4 text-center">
-        <div className="flex items-center">
-          <div className="text-sm">獲得ポイント：</div>
-          <div>
-            <span className="font-bold text-2xl">
-              {currentXp.toLocaleString()}
-            </span>
-            <span className="font-bold ml-0.5">ポイント</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ProgressBarSimple
+      current={progressInCurrentLevel}
+      max={xpRangeForCurrentLevel}
+    />
   );
 }
