8260425080d8665757c542ebc548778009d0fb6a
gray-code <koheiishido@gmail.com>
2025-06-11 20:59:17 +0900

    Githubでプルリクエストを出そうミッションの追加

---
+++ supabase/migrations/20250611060715_add_pr_mission.sql
@@ -0,0 +1,14 @@
+INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count, ogp_image_url, artifact_label)
+VALUES
+  (
+    gen_random_uuid(),
+    '開発者向け: Githubでプルリクエストを出そう',
+    '/img/mission_fallback.svg',
+    '<a href="https://policy.team-mir.ai/view/README.md" target="_blank" rel="noopener noreferrer">チームみらいのマニフェスト</a>を読んで、プルリクエストであなたの意見を提案しよう！チームみらいではマニフェストを皆さんと共に議論し、どんどんアップデート（改善）をしてゆきます。',
+    2,
+    NULL,
+    'LINK',
+    NULL,
+    '',
+    'あなたが作成したプルリクエストのURL'
+  )
