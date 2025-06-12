e6036af2cec37169ee87e2722da0b4498944929d
Seiichiro Tanaka <seiichi3141@gmail.com>
2025-06-12 22:16:00 +0900

    #379 リンクを別タブで開く

--- supabase/migrations/20250612120000_update_posting_mission_content.sql
+++ supabase/migrations/20250612120000_update_posting_mission_content.sql
@@ -5,7 +5,7 @@ SET
     content = 'チームみらいの機関誌を地域に配布してください。ポスティングした枚数と場所を報告してください。配布1枚につき5ポイントを獲得できます。
 
 詳しいポスティングの方法や注意点については、こちらのページをご確認ください：
-<a href="https://www.notion.so/team-mirai/206f6f56bae18049a2c2f989bc183a53?source=copy_link">機関誌ポスティング</a>
+<a target="_blank" href="https://team-mirai.notion.site/206f6f56bae18049a2c2f989bc183a53">機関誌ポスティング</a>
 
 活動前に必ずガイドラインをお読みいただき、適切な方法でポスティングを行ってください。',
     ogp_image_url = 'https://tibsocpjqvxxipszbwui.supabase.co/storage/v1/object/public/ogp//15_posting.png'
