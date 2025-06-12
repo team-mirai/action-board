10303605e3ed9949efe067218c075ad0f72778e2
YukiTsushima <>
2025-06-12 18:47:18 +0900

    画像の差し替え、required_artifact_typeの修正

--- supabase/migrations/20250611105031_add_join_speech.sql
+++ supabase/migrations/20250611105031_add_join_speech.sql
@@ -7,8 +7,8 @@ VALUES
     '「チームみらい」の街頭演説を一緒に盛り上げよう！あなたの声援が力になります。<br/><a href="https://team-mirai.notion.site/1fef6f56bae180fe9560d8ef50d3e44a?v=1fef6f56bae1802c8f15000c3f0195e0" target="_blank" rel="noopener noreferrer">イベントスケジュールはこちら</a>',
     1,
     NULL,
-    'LINK',
+    'TEXT',
     1,
-    'https://tibsocpjqvxxipszbwui.supabase.co/storage/v1/object/public/ogp//10_share_manifest_feedback.png', //TODO
+    'https://tibsocpjqvxxipszbwui.supabase.co/storage/v1/object/public/ogp//17_street_speech.png',
     '参加した演説の場所'
   )
