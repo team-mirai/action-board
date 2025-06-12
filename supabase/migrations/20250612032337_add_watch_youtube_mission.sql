b9cb5ed7fc883c8c5c631d567eccab763bdc46be
gray-code <koheiishido@gmail.com>
2025-06-12 15:25:57 +0900

    YouTube動画を視聴しようミッションの追加

---
+++ supabase/migrations/20250612032337_add_watch_youtube_mission.sql
@@ -0,0 +1,14 @@
+INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count, ogp_image_url, artifact_label)
+VALUES
+  (
+    gen_random_uuid(),
+    'YouTube動画を視聴しよう',
+    '/img/mission_fallback.svg',
+    '<a href="https://www.youtube.com/channel/UCiMwbmcCSMORJ-85XWhStBw" target="_blank" rel="noopener noreferrer">チームみらいの公式Youtubeチャンネル</a>でチームみらいの活動や政策解説、候補者のトーク、生配信アーカイブを視聴しよう！政治のことをわかりやすく解説してお届けしています。日本の政治を考えるヒントがきっと見つかるはず。まずは気になる動画から、ぜひご覧ください。',
+    1,
+    NULL,
+    'LINK',
+    NULL,
+    'https://tibsocpjqvxxipszbwui.supabase.co/storage/v1/object/public/ogp//20_youtube_view.png',
+    'あなたが視聴したYouTube動画のURL'
+  )
