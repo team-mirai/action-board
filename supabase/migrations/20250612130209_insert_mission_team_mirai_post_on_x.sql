d06d910fcf9c58533494e4372dd117af17ee4fbf
Naoki Makino <n.makino.0930@gmail.com>
2025-06-13 09:47:09 +0900

    chore: ミッションの説明文をいどばた政策ミッションと被らないよう変更
    - 登録値に以下のカラムを追加
      - is_featured
      - is_hidden

--- supabase/migrations/20250612130209_insert_mission_team_mirai_post_on_x.sql
+++ supabase/migrations/20250612130209_insert_mission_team_mirai_post_on_x.sql
@@ -1,16 +1,18 @@
 -- Xでチームみらいに関する投稿をしよう ミッションを追加
-INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count, ogp_image_url, artifact_label)
+INSERT INTO missions (id, title, icon_url, content, difficulty, event_date, required_artifact_type, max_achievement_count, ogp_image_url, artifact_label, is_featured, is_hidden)
 VALUES (
   '8f923e4a-bc5d-4a7e-9e1f-2c8d7b6a4e9f',
   'Xでチームみらいに関する投稿をしよう',
   '/img/mission_fallback.svg',
-  'チームみらいの活動や政策について、あなたの声をXで発信してみよう！<a href="https://policy.team-mir.ai/view/README.md">いどばた政策サイト</a>で見つけた政策への感想を投稿したり、イベント参加の感想を共有したり...たとえば『共感した』だけでもうれしいです。政治をもっと身近に感じる第一歩を踏み出そう！',
+  'チームみらいの活動について、あなたの声をXで発信してみよう！イベント参加の感想やチームみらいへの思い、何でも投稿してください。たとえば『共感した』だけでもうれしいです。政治をもっと身近に感じる第一歩を踏み出そう！',
   2,
   NULL,
   'LINK',
   NULL,
   'https://tibsocpjqvxxipszbwui.supabase.co/storage/v1/object/public/ogp//18_x_post.png',
-  '投稿したポストのURL')
+  '投稿したポストのURL',
+  false,
+  false)
 ON CONFLICT (id) DO UPDATE SET
   title = EXCLUDED.title,
   icon_url = EXCLUDED.icon_url,
@@ -20,4 +22,6 @@ ON CONFLICT (id) DO UPDATE SET
   required_artifact_type = EXCLUDED.required_artifact_type,
   max_achievement_count = EXCLUDED.max_achievement_count,
   ogp_image_url = EXCLUDED.ogp_image_url,
-  artifact_label = EXCLUDED.artifact_label;
+  artifact_label = EXCLUDED.artifact_label,
+  is_featured = EXCLUDED.is_featured,
+  is_hidden = EXCLUDED.is_hidden;
