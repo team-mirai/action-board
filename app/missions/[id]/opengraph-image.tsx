0648086c0c1c78855bc677bdbf5756a1c8709f48
Seiichiro Tanaka <seiichi3141@gmail.com>
2025-06-14 17:57:55 +0900

    ミッションのOGP画像の調整

--- app/missions/[id]/opengraph-image.tsx
+++ app/missions/[id]/opengraph-image.tsx
@@ -32,7 +32,7 @@ export default async function Image({ params }: { params: { id: string } }) {
   const pageData = await getMissionPageData(params.id);
 
   // ベース画像を読み込み
-  const baseImagePath = join(process.cwd(), "public/img/ogp-base.png");
+  const baseImagePath = join(process.cwd(), "public/img/ogo_mission_base.png");
   const baseImageBuffer = await readFile(baseImagePath);
   const baseImageBase64 = `data:image/png;base64,${baseImageBuffer.toString("base64")}`;
 
@@ -46,6 +46,7 @@ export default async function Image({ params }: { params: { id: string } }) {
         fontFamily: "Noto Sans JP",
         width: "100%",
         height: "100%",
+        padding: "40px",
         display: "flex",
         flexDirection: "column",
         alignItems: "flex-end",
@@ -57,51 +58,47 @@ export default async function Image({ params }: { params: { id: string } }) {
     >
       <div
         style={{
-          width: "60%",
-          fontSize: 48,
-          margin: "0 48px",
-          color: "black",
-          fontWeight: "700",
-          textAlign: "left",
-          borderRadius: "15px",
-          marginBottom: "20px",
-          wordWrap: "break-word",
-          whiteSpace: "pre-wrap",
-        }}
-      >
-        {titleWithLineBreak}
-      </div>
-      <div
-        style={{
-          fontFamily: "Noto Sans JP",
-          width: "60%",
-          fontSize: 28,
-          margin: "0 48px",
-          color: "black",
-          fontWeight: "700",
-          textAlign: "left",
-          borderRadius: "15px",
-          wordWrap: "break-word",
-        }}
-      >
-        #テクノロジーで誰も取り残さない日本へ
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          width: "100%",
-          bottom: "100px",
+          width: "62%",
           display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
           justifyContent: "center",
         }}
       >
-        <div style={{ display: "flex", alignItems: "baseline" }}>
+        <div
+          style={{
+            fontSize: 40,
+            color: "black",
+            fontWeight: "700",
+            marginBottom: "8px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {titleWithLineBreak}
+        </div>
+        <div
+          style={{
+            fontFamily: "Noto Sans JP",
+            fontSize: 28,
+            color: "black",
+            fontWeight: "700",
+            marginBottom: "24px",
+          }}
+        >
+          #テクノロジーで誰も取り残さない日本へ
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "baseline",
+          }}
+        >
           <div
             style={{
-              bottom: "48px",
               fontFamily: "Noto Sans JP",
-              fontSize: "72px",
-              color: "#059669",
+              fontSize: "58px",
+              color: "#0d9488",
               textAlign: "center",
               lineHeight: "1",
             }}
@@ -111,10 +108,9 @@ export default async function Image({ params }: { params: { id: string } }) {
           <div
             style={{
               marginLeft: "8px",
-              bottom: "48px",
               fontFamily: "Noto Sans JP",
-              fontSize: "32px",
-              color: "black",
+              fontSize: "24px",
+              color: "#0d9488",
               textAlign: "center",
             }}
           >
