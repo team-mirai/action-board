57ff8cf76b6b835ccc883d86af2951b91bd82bb5
o-ga09 <taitiabe1997@gmail.com>
2025-06-15 12:40:34 +0900

    fix: ミッションの達成履歴の文言を修正

--- app/missions/[id]/_components/SubmissionItem.tsx
+++ app/missions/[id]/_components/SubmissionItem.tsx
@@ -39,7 +39,7 @@ const SubmissionItem: React.FC<SubmissionItemProps> = ({
             size="xs"
             onClick={() => onCancelClick(submission.id)}
           >
-            取り消す
+            削除
           </Button>
         )}
       </div>
